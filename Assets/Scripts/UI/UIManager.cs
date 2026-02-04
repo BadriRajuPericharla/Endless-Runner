3f11fa1b460005bfaee85c6702fc695659624bfb
BadriRajuPericharla <bhagavanpericharla@gmail.com>
2026-02-04 10:22:42 +0530

    Resume CountDown Updated

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -1,4 +1,5 @@
 
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -6,6 +7,8 @@ public class UIManager : MonoBehaviour
 {
     [SerializeField]GameObject SettingsPannel;
     [SerializeField]GameObject MainMenuPannel;
+    [SerializeField]GameObject CountdownPannel;
+    [SerializeField]TMPro.TMP_Text CountdownText;
     static bool SkipMenu=false;
     void Start()
     {
@@ -17,6 +20,7 @@ public class UIManager : MonoBehaviour
         else
         {
             MainMenuPannel.SetActive(true);
+            //Time.timeScale=0f;
         }
     }
 
@@ -38,14 +42,28 @@ public class UIManager : MonoBehaviour
     public void ShowSettings()
     {
         SettingsPannel.SetActive(true);
+        //Time.timeScale=0f;
     }
     public void ResumeButton()
     {
-        SettingsPannel.SetActive(false);
+        StartCoroutine(ResumeCountdown());
     }
     public void HomeButton()
     {   
         SettingsPannel.SetActive(false);
         MainMenuPannel.SetActive(true);
+    }
+    IEnumerator ResumeCountdown()
+    {
+        SettingsPannel.SetActive(false);
+        CountdownPannel.SetActive(true);
+        CountdownText.text="3";
+        yield return new WaitForSeconds(1f);
+        CountdownText.text="2";
+        yield return new WaitForSeconds(1f);
+        CountdownText.text="1";
+        yield return new WaitForSeconds(1f);
+        CountdownPannel.SetActive(false);
+        //Time.timeScale=1f;
     }
 }
