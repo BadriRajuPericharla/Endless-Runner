c207156da0404afaa905ae344389b6823a50effd
BadriRajuPericharla <bhagavanpericharla@gmail.com>
2026-02-06 11:26:45 +0530

    Super Run Added

---
+++ Assets/Scripts/PowerUps/SuperRun.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class SuperRun : MonoBehaviour
+{
+    public Player player;
+    [SerializeField]float SpeedMultiplier=2f;
+    [SerializeField]float Duration=5f;
+    public float fadeAmount=0.5f;
+    float Original;
+    Material mat;
+    
+    void Start()
+    {
+        mat=GetComponent<Renderer>().material;
+        Original=mat.color.a;
+    }
+    
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "RunPower")
+        {
+            StartCoroutine(superrun());
+            Destroy(collision.gameObject);
+        }
+    }
+    IEnumerator superrun()
+    {
+        player.speed*=SpeedMultiplier;
+        Color c=mat.color;
+        c.a = fadeAmount;
+        mat.color = c;
+        
+        yield return new WaitForSeconds(Duration);
+        player.speed/=SpeedMultiplier;
+        c.a=Original;
+        mat.color=c;
+    }
+    
+}
